e132a3dace2e99e55eb490dbf32babeb0536f867
gustavocorrea86 <154375048+gustavocorrea86@users.noreply.github.com>
2024-01-09 09:49:24 -0300

    create arq

---
+++ desafios/desafio04/meuscript.js
@@ -0,0 +1,18 @@
+function gerar(){
+    var n1 = document.querySelector('#numero')
+    var res = document.querySelector('#res')
+    n1 = Number(n1.value) 
+    var c = ' '
+    var r = n1 * c
+    if(n1 == 0){
+        window.alert('Digite um número!!!')
+    }else{
+        res.innerHTML = `Tabuada do "${n1}" <br>`
+            for(var c = 0; c <=10; c++){
+                res.style.display = `block`
+                r = n1 * c 
+                res.innerHTML += `${n1} x ${c} = ${r}`
+                res.innerHTML +=`<br>`
+            }
+    }
+}
